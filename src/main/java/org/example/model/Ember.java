c35f0992f0b3fa9e2a1eee2c3024987b7585052c
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-10-12 08:55:45 +0200

    Setting up Ember class

---
+++ src/main/java/org/example/model/Ember.java
@@ -0,0 +1,47 @@
+package org.example.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ember {
+    private String name;
+    private int age;
+    private boolean speaksForeignLanguage;
+    private List<String> foreignLanguages;
+
+    public Ember(String name, int age, boolean speaksForeignLanguage) {
+        this.name = name;
+        this.age = age;
+        this.speaksForeignLanguage = speaksForeignLanguage;
+        this.foreignLanguages = new ArrayList<>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSpeaksForeignLanguage() {
+        return speaksForeignLanguage;
+    }
+
+    public void setSpeaksForeignLanguage(boolean speaksForeignLanguage) {
+        this.speaksForeignLanguage = speaksForeignLanguage;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void addLanguage(String language) {
+        foreignLanguages.add(language);
+    }
+}
